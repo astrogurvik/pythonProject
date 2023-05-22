414d262cdce33ba13a7754558d05f85167bb35b9
gk <gk@adamant.local>
2023-05-22 19:07:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+day = int(input('Введите номер дня недели:\n'))
+if day == 1:
+    print('Это понедельник')
+if day == 2:
+    print('Это вторник')
+if day == 3:
+    print('Это среда')
+if day == 4:
+    print('Это четверг')
+if day == 5:
+    print('Это пятница')
+if day == 6:
+    print('Это суббота')
+if day == 7:
+    print('Это воскресенье')
